d9151b2ef62a2f6aa35dc85bc935cf2f1003c4fc
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-01-11 16:18:37 +0530

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -22,11 +22,13 @@
                     $img_ext = end($img_explode);
     
                     $extensions = ["jpeg", "png", "jpg"];
+                    
                     if(in_array($img_ext, $extensions) === true){
                         $types = ["image/jpeg", "image/jpg", "image/png"];
                         if(in_array($img_type, $types) === true){
                             $time = time();
                             $new_img_name = $time.$img_name;
+
                             if(move_uploaded_file($tmp_name,"images/".$new_img_name)){
                                 $ran_id = rand(time(), 100000000);
                                 $status = "Active now";
