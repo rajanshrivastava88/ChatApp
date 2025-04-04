ab903313396305b8ce8776befac17e80ddb71ec8
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-04 18:46:07 +0530

    Update users.php

--- php/users.php
+++ php/users.php
@@ -1,22 +1,20 @@
 <?php
-    session_start();
 
-    include_once "config.php";
+session_start();
 
-    $outgoing_id = $_SESSION['unique_id'];
+include_once "config.php";
 
-    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
+$outgoing_id = $_SESSION['unique_id'];
 
-    $query = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
 
-    $output = "";
+$query = mysqli_query($conn, $sql);
 
-    if(mysqli_num_rows($query) == 0){
-        $output .= "No users are available to chat";
-    }
-    elseif(mysqli_num_rows($query) > 0)
-    {
-        include_once "data.php";
-    }
-    echo $output;
-?>
+$output = "";
+
+if (mysqli_num_rows($query) == 0) {
+    $output .= "No users are available to chat";
+} elseif (mysqli_num_rows($query) > 0) {
+    include_once "data.php";
+}
+echo $output;
