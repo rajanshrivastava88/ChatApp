617342afcb73e8f5b4350d00e0020cee5eb04c2c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-04 18:48:39 +0530

    Update users.php

--- users.php
+++ users.php
@@ -1,9 +1,9 @@
-<?php 
-  session_start();
-  include_once "php/config.php";
-  if(!isset($_SESSION['unique_id'])){
-    header("location: login.php");
-  }
+<?php
+session_start();
+include_once "php/config.php";
+if (!isset($_SESSION['unique_id'])) {
+  header("location: login.php");
+}
 ?>
 <?php include_once "header.php"; ?>
 
@@ -12,17 +12,17 @@
     <section class="users">
       <header>
         <div class="content">
-          <?php 
-            $sql = mysqli_query($conn, "SELECT * FROM users 
+          <?php
+          $sql = mysqli_query($conn, "SELECT * FROM users 
             WHERE unique_id = {$_SESSION['unique_id']}");
-            
-            if(mysqli_num_rows($sql) > 0){
-              $row = mysqli_fetch_assoc($sql);
-            }
+
+          if (mysqli_num_rows($sql) > 0) {
+            $row = mysqli_fetch_assoc($sql);
+          }
           ?>
           <img src="php/images/<?php echo $row['img']; ?>" alt="">
           <div class="details">
-            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+            <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
             <p><?php echo $row['status']; ?></p>
           </div>
         </div>
@@ -34,7 +34,7 @@
         <button><i class="fas fa-search"></i></button>
       </div>
       <div class="users-list">
-  
+
       </div>
     </section>
   </div>
@@ -42,4 +42,5 @@
   <script src="javascript/users.js"></script>
 
 </body>
+
 </html>
