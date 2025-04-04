3eccaf23edd3ff1209b641442ee33f571a87789e
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-04 18:47:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
-<?php 
-  session_start();
-  if(isset($_SESSION['unique_id'])){
-    header("location: users.php");
-  }
+<?php
+session_start();
+if (isset($_SESSION['unique_id'])) {
+  header("location: users.php");
+}
 ?>
 
 <?php include_once "header.php"; ?>
+
 <body>
   <div class="wrapper">
     <section class="form login">
@@ -28,9 +29,10 @@
       <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
     </section>
   </div>
-  
+
   <script src="javascript/pass-show-hide.js"></script>
   <script src="javascript/login.js"></script>
 
 </body>
+
 </html>
