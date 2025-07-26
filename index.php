f3ab8f86a16af75b977b75da89dc6a039056a80c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-07-26 14:06:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,20 +18,24 @@ if (isset($_SESSION['unique_id'])) {
             <label>First Name</label>
             <input type="text" name="fname" placeholder="First name" required>
           </div>
+
           <div class="field input">
             <label>Last Name</label>
             <input type="text" name="lname" placeholder="Last name" required>
           </div>
         </div>
+
         <div class="field input">
           <label>Email Address</label>
           <input type="text" name="email" placeholder="Enter your email" required>
         </div>
+
         <div class="field input">
           <label>Password</label>
           <input type="password" name="password" placeholder="Enter new password" required>
           <i class="fas fa-eye"></i>
         </div>
+        
         <div class="field image">
           <label>Select Image</label>
           <input type="file" name="image" accept="image/x-png,image/gif,image/jpeg,image/jpg" required>
