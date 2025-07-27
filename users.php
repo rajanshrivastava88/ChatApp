1ca783320a7f7f65da91b72fe5fc54b16d86a945
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-07-27 16:46:49 +0530

    Update users.php

--- users.php
+++ users.php
@@ -20,6 +20,7 @@ if (!isset($_SESSION['unique_id'])) {
             $row = mysqli_fetch_assoc($sql);
           }
           ?>
+          
           <img src="php/images/<?php echo $row['img']; ?>" alt="">
           <div class="details">
             <span><?php echo $row['fname'] . " " . $row['lname'] ?></span>
@@ -28,13 +29,14 @@ if (!isset($_SESSION['unique_id'])) {
         </div>
         <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
       </header>
+
       <div class="search">
         <span class="text">Select an user to start chat</span>
         <input type="text" placeholder="Enter name to search...">
         <button><i class="fas fa-search"></i></button>
       </div>
-      <div class="users-list">
 
+      <div class="users-list">
       </div>
     </section>
   </div>
