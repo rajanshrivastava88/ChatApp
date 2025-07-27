e05d5f0374d34876c22d514234f61761c79adf66
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-07-27 16:45:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ if (isset($_SESSION['unique_id'])) {
   <div class="wrapper">
     <section class="form signup">
       <header>Chatting App</header>
+
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        
         <div class="error-text"></div>
         <div class="name-details">
           <div class="field input">
